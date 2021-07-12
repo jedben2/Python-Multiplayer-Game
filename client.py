14e40bf754b156a0e5d2a837e9f13c102f42c779
James <jed.watson.jedw@gmail.com>
2021-07-12 17:04:27 +0100

    Client

--- client.py
+++ client.py
@@ -1,22 +1,22 @@
 import socket
 import threading
 
-host = '192.168.68.144'
-port = 65432
+host = '127.0.0.1'
+port = 2000
 
 
-def listener():
-    global s, connected
-    while connected:
-        data = s.recv(1024)
-        print("Received", repr(data))
+# def listener():
+#     global s, connected
+#     while connected:
+#         data = s.recv(1024)
+#         print("Received", repr(data))
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
     connected = True
-    x = threading.Thread(target=listener, args=(), daemon=True)
-    x.start()
+    # x = threading.Thread(target=listener, args=(), daemon=True)
+    # x.start()
     while connected:
         d = input().encode()
         if d.decode() == "exit":
