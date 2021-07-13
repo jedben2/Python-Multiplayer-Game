93ca5937a817c4fe94138346c3e149518afb64e3
James <jed.watson.jedw@gmail.com>
2021-07-13 20:59:59 +0100

    Big commit: player movement + walk animation on platform

--- game/main.py
+++ game/main.py
@@ -1,3 +1,4 @@
+import ursina
 from player import *
 
 app = Ursina()
@@ -5,32 +6,23 @@ app = Ursina()
 window.vsync = 60
 window.title = 'Multiplayer Game'
 window.borderless = False
+window.fullscreen_size = Vec2(1920, 1080)
 window.fullscreen = True
 window.exit_button.visible = True
 window.fps_counter.enabled = True
 
-dt = 1 / window.vsync
-
-# player = Player(0, 0, 'square')
-player = Player(position=(-5, 0), model="quad", texture="brick", collider='box')
-floor = Entity(position=(0, -2), model='quad', scale_x=30, collider='box')
+player = Player(position=(-5, 0), model="quad", texture="textures/char1_Right.png", collider='box', scale_x=1, scale_y=1)
+floor = Entity(position=(0, -1), model="quad", scale_x=30, collider='box')
 ursina.camera.position = (player.position.x, player.position.y, -40)
 
-collision_zone = CollisionZone(parent=player, radius=1)
+# collision_zone = CollisionZone(parent=player, radius=1)
 
-ursina.camera.zoom = -40
+ursina.camera.zoom = -30
+camera.add_script(SmoothFollow(target=player, offset=[0, 0, ursina.camera.zoom], speed=4))
 
 
 def update():
-    ursina.camera.zoom += held_keys["up arrow"] * 2 - held_keys["down arrow"] * 2
-    if ursina.camera.zoom > -10: ursina.camera.zoom = -10
-    if ursina.camera.zoom < -100: ursina.camera.zoom = -100
-    player.move()
-    player.check_collide(floor)
-
-    # player.update()
-    # print(player.position.x)
-    ursina.camera.position = (player.position.x, player.position.y, ursina.camera.zoom)
+    player.move(floor)
 
 
 app.run()
--- game/player.py
+++ game/player.py
@@ -1,8 +1,10 @@
 from ursina import *
+import threading
+import sys
 
 
 class Player(Entity):
-    def __init__(self, position, model, texture, collider):
+    def __init__(self, position, model, texture, collider, scale_x, scale_y):
         super().__init__()
         self.hp = 100
         self.dmg = 0
@@ -15,6 +17,10 @@ class Player(Entity):
         self.model = model
         self.texture = texture
         self.collider = collider
+        self.scale_x = scale_x
+        self.scale_y = scale_y
+
+        self.frame = 0
 
     def update(self):
         self.x += self.dx
@@ -24,14 +30,23 @@ class Player(Entity):
     def attack(self, other):
         pass
 
-    def move(self):
-        self.dx = 0
-        self.dy -= self.g * self.dt
-        self.dx += held_keys['d'] * 15 * self.dt - held_keys['a'] * 15 * self.dt
+    def move(self, other):
+        self.dx = held_keys['d'] * 15 * self.dt - held_keys['a'] * 15 * self.dt
+        if self.dx < 0:
+            if self.frame > 9:
+                self.frame = 0
+            if self.frame % 3 == 0: self.texture = f"animations//char1_walk_left//{self.frame // 3}.png"
+        elif self.dx > 0:
+            if self.frame > 9:
+                self.frame = 0
+            if self.frame % 3 == 0: self.texture = f"animations//char1_walk_right//{self.frame // 3}.png"
+        self.frame += 1
 
-    def check_collide(self, other):
-        self.y -= .5
+        self.dy -= self.g * self.dt
         if self.intersects(other).hit:
+            self.y = 0
+            self.dy = .75 * held_keys['w']
+        if self.y < -10:
+            self.y = 1
+            self.x = 0
             self.dy = 0
-        self.y += .5
-        return
